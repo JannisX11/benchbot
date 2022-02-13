7ea6ccc429a82efc24af1c46eb085053bd3b7b2f
Jannis P <jannis@spark-squared.com>
2022-02-13 18:12:53 +0100

    1.1.9 Remove game test spam

--- src/spam.js
+++ src/spam.js
@@ -1,12 +1,16 @@
 const {getChannel} = require('./util');
 
+function hasRole(member, role_name) {
+    return !!member.roles.cache.find(role => role.name == role_name);
+}
+
 module.exports = function DetectSpam(msg) {
     if (msg.content.match(/https?:\/\//i) &&
         (
             msg.content.match(/nitro/i) ||
             msg.content.match(/d.*\.gift\//i)
         ) &&
-        !msg.member.roles.cache.find(role => role.name == 'Moderator') &&
+        !hasRole(msg.member, 'Moderator') &&
         !msg.content.includes('.epicgames.com/') &&
         !msg.content.includes('.discord.com/')
     ) {
@@ -18,10 +22,26 @@ module.exports = function DetectSpam(msg) {
 			\`\`\`
         `.replace(/\t/g, ''));
 		return true;
-    } else if (msg.content.includes('@everyone') && !msg.member.roles.cache.find(role => role.name == 'Moderator')) {
+
+    } else if (msg.content.includes('@everyone') && !hasRole(msg.member, 'Moderator')) {
         msg.delete();
         msg.member.kick('Pinged everyone');
         getChannel('bot-log').send(`Deleted a message by ${msg.author} in #${msg.channel} attempting to ping everyone, and kicked user off the server.
+			\`\`\`
+			${msg.content.replace(/[´`]/g, "'")}
+			\`\`\`
+        `.replace(/\t/g, ''));
+		return true;
+
+    } else if (msg.content.match(/https?:\/\//i) &&
+        msg.content.match(/game/i) &&
+        msg.content.match(/play test|test play/i) &&
+        !msg.content.includes('map') &&
+        !hasRole(msg.member, 'Moderator')
+    ) {
+        msg.delete();
+        msg.member.kick('Game test spam');
+        getChannel('bot-log').send(`Deleted a message by ${msg.author} in #${msg.channel} and kicked user off the server. Likely game-test spam with a malicious link.
 			\`\`\`
 			${msg.content.replace(/[´`]/g, "'")}
 			\`\`\`
