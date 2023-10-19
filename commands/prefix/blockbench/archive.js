3d31739b88ac5121a500b09f61f15c0a44026432
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-10-19 12:59:42 +0100

    fix random archive error

--- commands/prefix/blockbench/archive.js
+++ commands/prefix/blockbench/archive.js
@@ -16,7 +16,7 @@ registerPrefixCommand(scriptName, prefixPath, {
     if (!args[1]) {
       [args[0], args[1]] = args[0].split(/(?<=^[^\n]*)\n/)
     }
-    const images = Array.from(message.attachments.filter(e => e.contentType.startsWith("image/"))).slice(0, 4)
+    const images = Array.from(message.attachments.filter(e => e.contentType?.startsWith("image/"))).slice(0, 4)
     if (!images.length) return sendError(message, {
       title: "Missing images",
       description: "Please provide at least one image"
