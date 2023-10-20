f81886bf9cbe954dba41c0553226d9111ffa926e
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-10-20 09:45:56 +0100

    add replacements to FAQ titles

--- functions/messages/makeFAQ.js
+++ functions/messages/makeFAQ.js
@@ -1,8 +1,18 @@
+const replacements = {
+  "3d": "3D",
+  "3 d": "3D",
+  "uv": "UV"
+}
+
+const pattern = new RegExp(`\\b(${Object.keys(replacements).join("|")})\\b`, "gi")
+
+const replace = (a, b) => replacements[b.toLowerCase()]
+
 registerFunction(scriptName, data => {
   if (data.data.embedless) return { content: `${data.text}${data.data.image ? `\n\n${data.data.image}` : ""}` }
   return {
     author: ["FAQ", client.icons.faq],
-    title: `${data.category.toTitleCase(true, true)}: ${data.id.toTitleCase(true, true)}`,
+    title: `${data.category.toTitleCase(true, true).replace(pattern, replace)}: ${data.id.toTitleCase(true, true).replace(pattern, replace)}`,
     description: data.text,
     image: data.data.image,
     deletable: true
