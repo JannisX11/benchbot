f1364c8657d4a9f0f7269a26812ea27d5a574bc0
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-10-23 16:33:31 +0100

    fix avatar function

--- functions/images/avatar.js
+++ functions/images/avatar.js
@@ -1,7 +1,9 @@
 registerFunction(scriptName, (member, size = 1024, args) => {
   if (!member) return `https://cdn.discordapp.com/embed/avatars/${randInt(5)}.png`
-  return member.displayAvatarURL({
+  let url = member.displayAvatarURL({
     extension: getType.image("PNG"),
     forceStatic: args?.static
-  }) + `?size=${size}`
+  })
+  if (size) url += `?size=${size}`
+  return url
 })
