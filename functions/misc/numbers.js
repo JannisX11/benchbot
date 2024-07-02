1971f54be44c4bf05c063e0ad3bb7fd4b517d4eb
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-07-02 10:33:58 +0100

    fix spamcheck and systemstats

--- functions/misc/numbers.js
+++ functions/misc/numbers.js
@@ -9,5 +9,7 @@ registerFunction(scriptName, {
     const minutes = Math.floor(num / 6e4) % 60
     const seconds = Math.round(num / 1000 % 60)
     return `${years} year${years === 1 ? "" : "s"}, ${weeks} week${weeks === 1 ? "" : "s"}, ${days} day${days === 1 ? "" : "s"}, ${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`.replace(/(?<!\d)0\s[a-z]+,\s/g, "").replace(/(, 00:00:00)/, "")
-  }
+  },
+  formatPercentage: percentage => percentage.toString().replace(/\.?0+$/, "") + "%",
+  percentage: (val, total) => formatPercentage(((val / total) * 100).toFixed(2))
 })
